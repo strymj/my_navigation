22d01f83dfd10fdac693cd1b02071c532478dba8
strymj <yamaji.shoutarou@gmail.com>
2017-06-01 00:53:34 +0900

    update

--- src/mark_detector.cpp
+++ src/mark_detector.cpp
@@ -2,6 +2,9 @@
 #include <ros/ros.h>
 #include <iostream>
 #include <string>
+#include <dynamic_reconfigure/DoubleParameter.h>
+#include <dynamic_reconfigure/Reconfigure.h>
+#include <dynamic_reconfigure/Config.h>
 /*}}}*/
 
 int main (int argc, char **argv)
@@ -10,32 +13,41 @@ int main (int argc, char **argv)
 	ros::NodeHandle nh("~");
 	ros::Rate looprate (30);
 
-	double time = 0.0;
 	int cnt = 0;
-	double vel = 0.0;
+	double vel = 0.3;
+
+	dynamic_reconfigure::ReconfigureRequest srv_req;
+	dynamic_reconfigure::ReconfigureResponse srv_resp;
+	dynamic_reconfigure::DoubleParameter double_param;
+	dynamic_reconfigure::Config conf;
+
+	double_param.name = "max_vel_x";
+	double_param.value = vel;
+	conf.doubles.push_back(double_param);
+	srv_req.config = conf;
 
 	while (ros::ok())
 	{/*{{{*/
-		if (10 * 30<cnt)
+		if (10*30 < cnt)
 		{
-			nh.setParam("/move_base/TrajectoryPlannerROS/max_vel_x", 0.9);
-			ROS_INFO("changed param!");
-			nh.getParam("/move_base/TrajectoryPlannerROS/max_vel_x", vel);
-			ROS_INFO("max_vel_x : %f", vel);
+			vel = 0.3;
+			conf.doubles[0].value = vel;
+			srv_req.config = conf;
+			ros::service::call("/move_base/TrajectoryPlannerROS/set_parameters", srv_req, srv_resp);
+			ROS_INFO("changed param   max_vel_x = %f", vel);
 			cnt = 0;
 
 		}
 		else if (cnt == 5*30) 
 		{
-			// nh.setParam("/hogehoge/piyopiyo", 0.5);
-			nh.setParam("/move_base/TrajectoryPlannerROS/max_vel_x", 0.3);
-			ROS_INFO("changed param!");
-			nh.getParam("/move_base/TrajectoryPlannerROS/max_vel_x", vel);
-			ROS_INFO("max_vel_x : %f", vel);
+			vel = 0.9;
+			conf.doubles[0].value = vel;
+			srv_req.config = conf;
+			ros::service::call("/move_base/TrajectoryPlannerROS/set_parameters", srv_req, srv_resp);
+			ROS_INFO("changed param   max_vel_x = %f", vel);
 		}
 		ros::spinOnce();
 		looprate.sleep();
-		time += 1.0/30;
 		++cnt;
 	}/*}}}*/
 
