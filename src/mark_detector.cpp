7dff8c0d50d9cab38f664762f9fa5697bc9d4495
strymj <yamaji.shoutarou@gmail.com>
2017-05-31 19:45:58 +0900

    update

--- src/mark_detector.cpp
+++ src/mark_detector.cpp
@@ -10,21 +10,22 @@ int main (int argc, char **argv)
 	ros::NodeHandle nh("~");
 	ros::Rate looprate (30);
 
-	double time = 0;
+	double time = 0.0;
+	int cnt = 0;
 	double vel = 0.0;
 
 	while (ros::ok())
 	{/*{{{*/
-		if (10<time)
+		if (10 * 30<cnt)
 		{
 			nh.setParam("/move_base/TrajectoryPlannerROS/max_vel_x", 0.9);
 			ROS_INFO("changed param!");
 			nh.getParam("/move_base/TrajectoryPlannerROS/max_vel_x", vel);
 			ROS_INFO("max_vel_x : %f", vel);
-			time = 0.0;
+			cnt = 0;
 
 		}
-		else if (5<time) 
+		else if (cnt == 5*30) 
 		{
 			// nh.setParam("/hogehoge/piyopiyo", 0.5);
 			nh.setParam("/move_base/TrajectoryPlannerROS/max_vel_x", 0.3);
@@ -35,6 +36,7 @@ int main (int argc, char **argv)
 		ros::spinOnce();
 		looprate.sleep();
 		time += 1.0/30;
+		++cnt;
 	}/*}}}*/
 
 	return 0;
