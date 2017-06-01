a54ed763b3b84d18d176b40707f2e762225cb58a
strymj <yamaji.shoutarou@gmail.com>
2017-06-01 15:29:48 +0900

    update

--- src/mark_detector.cpp
+++ src/mark_detector.cpp
@@ -5,13 +5,19 @@
 #include <dynamic_reconfigure/DoubleParameter.h>
 #include <dynamic_reconfigure/Reconfigure.h>
 #include <dynamic_reconfigure/Config.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <mylib/ImageProc.h>
+using namespace std;
+using namespace cv;
 /*}}}*/
 
 int main (int argc, char **argv)
 {/*{{{*/
 	ros::init (argc, argv, "mark_detector");
 	ros::NodeHandle nh("~");
-	ros::Rate looprate (30);
+	ros::Rate looprate (10);
 
 	int cnt = 0;
 	double vel = 0.3;
@@ -26,8 +32,60 @@ int main (int argc, char **argv)
 	conf.doubles.push_back(double_param);
 	srv_req.config = conf;
 
+	// string filepath = "/home/strymj/Pictures/sokudo.jpg";
+	// string filepath = "/home/strymj/Pictures/so.png";
+	string filepath = "/home/strymj/Pictures/vel20.jpg";
+	bool picture = true;
+	cv::Mat frame;
+
+	if (picture) 
+	{
+		frame = cv::imread(filepath);
+		if (!frame.data)
+		{
+			ROS_ERROR("cannot read image");
+			return -1;
+		}
+	}
+	cv::VideoCapture cap(1);
+	if(!cap.isOpened())
+		return -1;
+
+
+
+	my::Image::HSV hsv(160,40, 100,255, 190,255);
+	Mat extract;
+	Mat cut;
+
+
 	while (ros::ok())
 	{/*{{{*/
+
+		if (!picture)
+			cap >> frame;
+		my::Image::colorExtract(frame, extract, hsv, 0);
+		vector<my::Image::Regiondata> regionlist;
+		my::Image::labeling(extract, regionlist);
+
+		int max_num = -1;
+		int pix = 0;
+		for (int i=0; i<regionlist.size(); ++i)
+		{
+			if (pix<regionlist[i].pixels)
+			{
+				pix = regionlist[i].pixels;
+				max_num = i;
+			}
+		}
+		if (max_num != -1)
+		{
+			Mat cut_img(frame,Rect(regionlist[max_num].min.x,regionlist[max_num].min.y,regionlist[max_num].size.x,regionlist[max_num].size.y));
+			cv::imshow("cut", cut_img);
+		}
+		cv::imshow("webcam", frame);
+		cv::imshow("extracted", extract);
+		cv::waitKey(1);
+
 		if (10*30 < cnt)
 		{
 			vel = 0.3;
